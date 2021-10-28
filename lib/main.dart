bee31cb8a4a4512951747284a5ecfdc9777f8eb8
Yennhi Dang <nhidangn@gmail.com>
2021-10-28 14:19:34 -0700

    removed dropdown dependency

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
     this.text = text;
     income = double.parse(this.text);
     setState(() {
-      remain = income - 5;
+      remain = income;
     });
   }
 
@@ -55,214 +55,216 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: Column(
-          children: <Widget>[
-            Stack(
-              children: [
-                Container(
-                    height: 230,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        bottomRight: Radius.circular(50),
-                      ),
-                        gradient: LinearGradient(
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.topRight,
-                            colors: [Colors.blue, Colors.white]
+      body: SingleChildScrollView(
+        child: Column(
+            children: <Widget>[
+              Stack(
+                children: [
+                  Container(
+                      height: 230,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(50),
                         ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.blue.withOpacity(0.4),
-                            spreadRadius: 7,
-                            blurRadius: 30,
-                            offset: Offset(0, 3)
-                          )
-                        ]
-                    )
-                ),
-                Container(
-                  margin: EdgeInsets.only(top:150, left:40),
-                  child: Text(
-                    'My Spending',
-                        style: TextStyle(fontSize: 35, color: Colors.white, fontWeight: FontWeight.w600),
-                  )
-                )
-              ],
-            ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(left: 60, top: 40),
-                    child: Text(
-                      'Income',
-                      style: TextStyle(fontSize: 23),
-                    ),
-                       ),
-                  Container(
-                    margin: EdgeInsets.only(left: 50,top: 40),
-                    child: Icon(
-                      Icons.attach_money,
-                      size: 35,
-                      color: Colors.grey
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(right: 60, top: 40),
-                    width: 100,
-                      child: TextField(
-                        style: TextStyle(fontSize: 23),
-                        textAlign: TextAlign.center,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.only(top: 10),
-                            isDense: true,
-                            filled: true,
-                            fillColor: Colors.blue.shade100,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide.none
-                            ),
-                            counterText: ""
-                        ),
-                        onChanged: (text) => this.calculateRemaining(text)
-                      ),
-                     ),
-                ],
-              ),
-             Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                  Container(
-                      margin: EdgeInsets.only(left: 60),
-                      child: Text(
-                        'Remaining',
-                        style: TextStyle(fontSize: 23),
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomLeft,
+                              end: Alignment.topRight,
+                              colors: [Colors.blue, Colors.white]
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.blue.withOpacity(0.4),
+                              spreadRadius: 7,
+                              blurRadius: 30,
+                              offset: Offset(0, 3)
+                            )
+                          ]
                       )
                   ),
+                  Container(
+                    margin: EdgeInsets.only(top:150, left:40),
+                    child: Text(
+                      'My Spending',
+                          style: TextStyle(fontSize: 35, color: Colors.white, fontWeight: FontWeight.w600),
+                    )
+                  )
+                ],
+              ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
                     Container(
-                      margin: EdgeInsets.only(left: 14),
+                      margin: EdgeInsets.only(left: 60, top: 40),
+                      child: Text(
+                        'Income',
+                        style: TextStyle(fontSize: 23),
+                      ),
+                         ),
+                    Container(
+                      margin: EdgeInsets.only(left: 50,top: 40),
                       child: Icon(
-                          Icons.attach_money,
-                          size: 35,
+                        Icons.attach_money,
+                        size: 35,
                         color: Colors.grey
                       ),
                     ),
-                  Container(
-                    margin: EdgeInsets.only(top: 7, right: 60),
-                    width: 100,
-                    height: 39,
-                    decoration: BoxDecoration(
-                      color: Colors.blue.shade100,
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                    child: Center(
-                      child: Text(
-                        remain == null ? '0' : remain.toString(),
-                        style: TextStyle(
-                            fontSize: 23),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-
-                ]
-              ),
-
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: 250,
-                  margin: EdgeInsets.only(top:40, left: 47),
-                  child: TextField(
-                    style: TextStyle(fontSize: 16),
-                    controller: getCategory,
-                    textCapitalization: TextCapitalization.words,
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(10),
-                        filled: true,
-                        fillColor: Colors.blue.shade50,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide.none
-                        ),
-                        hintText: "Category e.g. McDonald's"
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 47, top: 20),
-                  width: 300,
-                  child: TextField(
-                    controller: getNotes,
-                    obscureText: false,
-                    maxLines: 4,
-                    textCapitalization: TextCapitalization.sentences,
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(10),
-                        filled: true,
-                        fillColor: Colors.blue.shade50,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide.none
-                        ),
-                        hintText: "Notes"
-                    ),
-                  ),
-                ),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(top: 15),
-                        child: Text (
-                          'Total',
-                          style: TextStyle(fontSize: 23),
-                        ),
-                      ),
-                      Container(
-                        width: 200,
-                        margin: EdgeInsets.only(top: 20, left: 30, right: 47),
+                    Container(
+                      margin: EdgeInsets.only(right: 60, top: 40),
+                      width: 100,
                         child: TextField(
-                          controller: getTotal,
-                          keyboardType: TextInputType.number,
                           style: TextStyle(fontSize: 23),
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.number,
                           decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.attach_money),
-                              contentPadding: EdgeInsets.all(10),
+                            contentPadding: EdgeInsets.only(top: 10),
+                              isDense: true,
                               filled: true,
-                              fillColor: Colors.blue.shade50,
+                              fillColor: Colors.blue.shade100,
                               border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide.none
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide.none
                               ),
                               counterText: ""
                           ),
+                          onChanged: (text) => this.calculateRemaining(text)
+                        ),
+                       ),
+                  ],
+                ),
+               Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                    Container(
+                        margin: EdgeInsets.only(left: 60),
+                        child: Text(
+                          'Remaining',
+                          style: TextStyle(fontSize: 23),
+                        )
+                    ),
+                      Container(
+                        margin: EdgeInsets.only(left: 14),
+                        child: Icon(
+                            Icons.attach_money,
+                            size: 35,
+                          color: Colors.grey
                         ),
                       ),
-                    ]
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.only(top: 30),
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      textStyle: const TextStyle(fontSize: 20),
+                    Container(
+                      margin: EdgeInsets.only(top: 7, right: 60),
+                      width: 100,
+                      height: 39,
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade100,
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Center(
+                        child: Text(
+                          remain == null ? '0' : remain.toString(),
+                          style: TextStyle(
+                              fontSize: 23),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SummaryPage()),
-                      );
-                    },
-                    child: const Text('Add Expense'),
-                  ),
-                ),
-              ],
-            )
 
-      ]
+                  ]
+                ),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 250,
+                    margin: EdgeInsets.only(top:80, left: 47),
+                    child: TextField(
+                      style: TextStyle(fontSize: 16),
+                      controller: getCategory,
+                      textCapitalization: TextCapitalization.words,
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(10),
+                          filled: true,
+                          fillColor: Colors.blue.shade50,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none
+                          ),
+                          hintText: "Category e.g. McDonald's"
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 47, top: 20),
+                    width: 300,
+                    child: TextField(
+                      controller: getNotes,
+                      obscureText: false,
+                      maxLines: 4,
+                      textCapitalization: TextCapitalization.sentences,
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(10),
+                          filled: true,
+                          fillColor: Colors.blue.shade50,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none
+                          ),
+                          hintText: "Notes"
+                      ),
+                    ),
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 15),
+                          child: Text (
+                            'Total',
+                            style: TextStyle(fontSize: 23),
+                          ),
+                        ),
+                        Container(
+                          width: 200,
+                          margin: EdgeInsets.only(top: 20, left: 30, right: 47),
+                          child: TextField(
+                            controller: getTotal,
+                            keyboardType: TextInputType.number,
+                            style: TextStyle(fontSize: 23),
+                            decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.attach_money),
+                                contentPadding: EdgeInsets.all(10),
+                                filled: true,
+                                fillColor: Colors.blue.shade50,
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide.none
+                                ),
+                                counterText: ""
+                            ),
+                          ),
+                        ),
+                      ]
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(top: 30, bottom:30),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SummaryPage()),
+                        );
+                      },
+                      child: const Text('Add Expense'),
+                    ),
+                  ),
+                ],
+              )
+
+        ]
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed:() {
