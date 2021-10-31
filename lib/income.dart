a4854236c84a08e1dfe593d268d0956106032bcc
Yennhi Dang <nhidangn@gmail.com>
2021-10-30 21:48:22 -0700

    added ui for Income page

--- lib/income.dart
+++ lib/income.dart
@@ -21,27 +21,70 @@ class _IncomePageState extends State<IncomePage> {
     return Scaffold(
       body: Column(
         children: [
-          Text(
-            "Income"
-          ),
-          TextField(
-            controller: incomeInput,
-            textAlign: TextAlign.center,
-            keyboardType: TextInputType.number,
-          ),
-          TextButton(
-            style: TextButton.styleFrom(
-              textStyle: const TextStyle(fontSize: 20),
+          Container(
+            alignment: Alignment.topLeft,
+            margin: EdgeInsets.only(top: 50, left: 30),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                Icons.arrow_back_rounded,
+                color: Colors.grey,
+                size: 35,
+              ),
             ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => MyHomePage(title: '')),
-              );
-            },
-            child: const Text('Save'),
           ),
-        ]
+          Center(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 50),
+                  child: Text(
+                    "Income",
+                      style: TextStyle(
+                          fontWeight:FontWeight.w700,
+                          fontSize: 30,
+                    )
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 30, bottom: 20),
+                  width: 300,
+                  child: TextField(
+                    controller: incomeInput,
+                    style: TextStyle(fontSize: 23),
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(top: 20, bottom: 20),
+                        isDense: true,
+                        filled: true,
+                        fillColor: Colors.blue.shade100,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none
+                        ),
+                        counterText: ""
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MyHomePage(title: '')),
+                    );
+                  },
+                  child: const Text('Save'),
+                ),
+              ]
+            ),
+          ),
+        ],
       )
     );
   }
